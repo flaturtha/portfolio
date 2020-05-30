0ff5a445f4f76aaff260a245ea06cd8f64993819
flaturtha <rich@talesofmurder.com>
2020-05-30 17:23:25 -0500

    adds content

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
       </div>
       <div className="mt-4 lg:mt-0 lg:mx-auto lg:flex lg:flex-col">
         <h1 className="text-center text-3xl lg:text-6xl">I'm a writer &hellip;</h1>
-        <h2 className="text-sm text-gray-400 lg:text-base">content marketing &middot; prose fiction &middot; screenwriting</h2>
+        <h2 className="text-sm text-shark-300 lg:text-base">content marketing &middot; prose fiction &middot; screenwriting</h2>
       </div>
     </div>
 
@@ -22,12 +22,38 @@ const IndexPage = () => (
 
     <div className="container mx-auto mt-4 p-6 lg:w-1/2">
       <p>I wrote my first book in the 4th grade, a magical little thing called <em>The Wide World of Sharks</em>, a #1 Bestseller in its category (books written by me in the 4th grade) and sold out its 1st printing (of one copy).</p>
-      <p className="italic">But that was the beginning of a writing career &hellip;</p>
-      <div className="text-white bg-shark-400 h-64 w-40 border mx-auto mt-8 p-2 pt-20 font-bold text-center">THE WIDE WORLD OF SHARKS</div>
+      <div className="text-shark-100 bg-shark-400 h-64 w-40 border mx-auto mt-8 p-2 pt-20 font-bold text-center">THE WIDE WORLD OF SHARKS</div>
+      <p className="italic lg:text-center mt-8 mb-0">But that was the beginning of a writing career &hellip;</p>
     </div>
 
-    <hr className="mt-4 text-shark-300 w-1/2 mx-auto" />
+    <hr className="my-4 lg:mb-8 text-shark-300 w-1/2 mx-auto" />
 
+    <div className="flex flex-col lg:flex-row">
+      <section className="border m-4 p-4 bg-shark-100 shadow-xl">
+        <h2 className="font-black text-center mb-4">COPYWRITING</h2>
+        <p>For more than 20 years, I've worked for a variety of business, corporations, trade &amp; government organizations providing copywriting services including email copywriting, newsletter &amp; magazine writing/editing, video ad scripts, and more. For details and to see examples of my work, check out <a href="/copywriting" className="underline uppercase italic">Copywriting</a></p>
+      </section>
+
+      <section className="border m-4 p-4 bg-shark-100 shadow-xl">
+        <h2 className="font-black text-center mb-4">PROSE FICTION</h2>
+        <p>Storytelling has always been my first love in writing, starting when I penned short stories in high school. In 2012, I finished a 1st draft of my hardboiled detective novel (soon to be a series) called <span className="font-bold uppercase">The Joe Sputnik Chronicles</span>. I am currently (as of 2020) working on a cosmic horror series called <span className="font-bold uppercase">The 64 Testaments of Col. Krill</span>. Learn more about my <a href="/fiction" className="underline uppercase italic">novels</a>.</p>
+      </section>
+      
+      <section className="border m-4 p-4 bg-shark-100 shadow-xl">
+        <h2 className="font-black text-center mb-4">SCREENWRITING</h2>
+        <p>I discovered the unique style of writing screenplays in college and have dabbled in the form ever since. To date, I've completed three spec scripts: (1) <span className="font-bold uppercase">The Illustrated Man</span>, [2] <span className="font-bold uppercase">The Geriatric Squad</span>, and [3] <span className="font-bold uppercase">The Briny Deep</span>. Learn more about my <a href="/screenwriting" className="underline uppercase italic">screenplays</a>.</p>
+      </section>
+    </div>
+
+    <hr className="my-8 text-shark-300 w-1/2 mx-auto" />
+
+    <div className="mb-8 p-6 pt-0">
+      <h2 className="font-bold text-2xl mb-2">My story &rarr;</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo at magnam unde debitis quibusdam, architecto a voluptatem enim eos corporis, atque ipsum tempora sunt eligendi tempore labore veritatis! Vero, nobis.</p>
+      <p>Odit, dicta cupiditate quo earum unde eos aliquid fugiat consequuntur, vero qui ut distinctio quasi molestias corrupti amet atque voluptates sed praesentium facilis expedita similique quod in accusantium. Expedita, dolor!</p>
+      <p>Assumenda quidem quia rerum cumque sint libero incidunt, cum quos ab dolorem nulla excepturi hic nesciunt odio dignissimos, distinctio perferendis architecto consequatur esse id. Minima distinctio voluptatem quam beatae molestias!</p>
+      <p className="underline font-medium">View my full resume as a PDF file.</p>  
+    </div>
 
   </Layout>
 )
--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -10,7 +10,27 @@ import SEO from "../components/seo"
 const TestimonialsPage = () => (
   <Layout>
     <SEO title="testimonials" />
-    <h1>Enough about me &hellip; what others have said about me!</h1>
+    <h1 className="text-3xl font-black mb-8 pl-2 lg:pl-0 lg:text-center">Enough about me &hellip; what others have said about me!</h1>
+    <div className="flex flex-col w-3/4 mx-auto">
+      <p>“<em>I need to bring Rich in to teach my editors how to run a magazine.</em>”<br/>&mdash; Ron Higgins, Publisher, World Oil</p>
+
+      <p>“<em>Rich did a great job on a sales letter I’d struggled with for awhile. He delivered a great letter on time and to my specifications. Thanks, Rich.</em>”<br/>&mdash; Phillip Cordova, Chiropractor</p>
+      
+      <p>“<em>Rich was a pleasure to work with the 5+ years that we worked together on the Momentum Magazine, where Rich was the Editor. His skills at writing and photography were top notch, and his attitude and demeanor made each issue a pleasure to produce.</em>”<br/>&mdash; Curtis Weeks, Business Development Director at RR Donnelley</p>
+
+      <p><em>I met Rich via a digital marketing course we both took, where I learned that he's a writer.
+
+      I reached out to him subsequently with an idea for a video series I thought might be interesting. Gave him a very rough outline and set of ideas, and by the next day, he had a concept pulled together.
+      
+      It wasn't quite what I was looking for. Based on my comments, he came back again a day later with a revised concept and a pretty much full dialogue script. And, frankly, he pretty much nailed it. I couldn't believe how well he heard me (and it was via text... so that adds to the remarkableness!)
+      
+      I'm producing the concept he/we came up with, am super-excited about it, and can't wait to throw some ideas at him for vid #2 of this series!</em><br/>&mdash; John Snippe, H2Only</p>
+
+      <p><em>As part of a writing assignment, Rich and I swapped treatments. Although my main genre is fantasy, I wrote a thriller. Rich immediately started filling in gaps and creating segues for me in places where I was struggling. Adding elements that I hadn't considered like an addition to my opening scene, enhanced my story. He even went so far as to change a situation that would reduce the budget yet keep the impact that I wanted for that scene.
+
+      Rich was easy to work with, encouraging, and willing to take the time to help me get my script to a much higher level. I would co-write with him anytime.</em><br/>&mdash; Ann Jagger, screenwriter</p>
+    </div>
+
   </Layout>
 )
 
